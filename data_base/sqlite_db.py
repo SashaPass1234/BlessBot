a5621f31c8d81d0909008ab393f0fab9d9ac400f
PASIK COM <140898404+SashaPass1234@users.noreply.github.com>
2024-11-08 18:34:44 +0300

    Update sqlite_db.py

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -2,7 +2,7 @@ import sqlite3 as sq
 
 def sql_start():
 	global base, cur
-	base = sq.connect('shop.db')
+	base = sq.connect('name_data_base.db') #NAME YOUR DATABASE 
 	cur = base.cursor()
 	if base:
 		print('Data base connected OK!')
