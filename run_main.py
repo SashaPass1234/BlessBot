158752d25125256f2682c72ea125f181c6b34365
PASIK COM <140898404+SashaPass1234@users.noreply.github.com>
2024-11-08 18:31:03 +0300

    Update run_main.py

--- run_main.py
+++ run_main.py
@@ -6,12 +6,8 @@ from handlers import client, admin
 async def on_startup(_):	
 	sqlite_db.sql_start()
 
-
-
 admin.register_handlers_admin(dp)
 client.register_handlers_client(dp)
 
-
-	
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
